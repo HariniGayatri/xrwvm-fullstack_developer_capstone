4b22c42edbeeaf66e09c3d299729569ad81fce17
HariniGayatri <148984516+HariniGayatri@users.noreply.github.com>
2024-04-13 18:51:00 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,6 +13,17 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='registration', view=views.registration, name='register'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path('dealerships/', views.get_dealerships, name='dealerships'),
+
+    # path for dealer reviews view
+    path('dealer_reviews/<int:dealer_id>/', views.get_dealer_reviews, name='dealer_reviews'),
+
+    # path for dealer details view
+    path('dealer_details/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+
+    # path for add a review view
+    path('add_review/', views.add_review, name='add_review'),
     # path for dealer reviews view
 
     # path for add a review view
